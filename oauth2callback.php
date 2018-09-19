42ecacd340e63e1c60468fe5275d3ce8033c1d9e
RajicDenis <denis.rajic92@gmail.com>
2018-09-20 00:05:28 +0200

    Finalize

--- oauth2callback.php
+++ oauth2callback.php
@@ -12,12 +12,15 @@ $client->setAccessType('offline');
 $client->setApprovalPrompt('force');
 
 if (! isset($_GET['code'])) {
+
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+
 } else {
+
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
   $redirect_uri = APPROOT;
-  //$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/YoutubeAPI';
+
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
